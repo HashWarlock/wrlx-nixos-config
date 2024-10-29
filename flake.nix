08fc24f7a59eee9803984150f5fe62a82d0faba9
HashWarlock <hashwarlock@protonmail.com>
2024-10-28 22:01:54 -0500

    Temp fix until refactor

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
+            home-manager.useGlobalPkgs = false;
             home-manager.useUserPackages = true;
 
             home-manager.extraSpecialArgs = inputs // specialArgs;
